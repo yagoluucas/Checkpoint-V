9a25064be93e51f774d491f500a220afb1d215b9
Yago Lucas <yago543@gmail.com>
2024-05-02 09:17:42 -0300

    feito rodapé da página

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,8 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={inter.className}>
         <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import './syle.css'
 
-export default function Header(props) {
+export default function Header() {
     return (
-        <header>
-            <Link href={'/'}><Image src={'/logo.png'} alt="Logo da empresa" width={150} height={200}/></Link>
+        <header className="header-pagina">
+            <Link href={'/'}><Image className="img-logo" src={'/logo.png'} alt="Logo da empresa" width={150} height={150}/></Link>
+            <nav>
+                <ul className="nav-header">
+                    <li><Link className="link" href={'/contato'}>contato</Link></li>
+                    <li><Link className="link" href={'/produtos'}>produtos</Link></li>
+                    <li><Link className="link" href={'/sobre'}>sobre</Link></li>
+                </ul>
+            </nav>
         </header>
     )
 }
