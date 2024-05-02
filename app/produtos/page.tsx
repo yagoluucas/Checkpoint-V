6edcd84c39a5ccf43dca9224761507ca110921b8
Nicolas Martins <nimartins.na1@gmail.com>
2024-05-02 11:55:50 -0300

    ajeitando a estilização da pagina de produtos

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -1,14 +1,49 @@
 import ProductCard from "@/components/ProductCard";
 
-
-export default function Produtos (){
+export default function Produtos() {
     return (
-        <div>
-            <ProductCard
-                img="/bmw.png"
-                title="BMW muito bonita"
-                price={10000000000000}
-                description="essa BMW eh linda"/>
-        </div>
+        <main>
+            <section className="flex flex-wrap">
+                <ProductCard
+                    img="/bmw.png"
+                    title="BMW muito bonita"
+                    price={10000000000000}
+                    description="essa BMW eh linda" />
+                <ProductCard
+                    img="/bmw2.png"
+                    title="BMW mais bonita ainda"
+                    price={151515}
+                    description="essa bmw possui 4 pneus" />
+
+                <ProductCard
+                    img="/bmw3.png"
+                    title="BMW modelo novo 2030"
+                    price={151515}
+                    description="essa BMW também tem 4 pneus" />
+                    <ProductCard
+                    img="/bmw2.png"
+                    title="BMW mais bonita ainda"
+                    price={151515}
+                    description="essa bmw possui 4 pneus" />
+
+                <ProductCard
+                    img="/bmw3.png"
+                    title="BMW modelo novo 2030"
+                    price={151515}
+                    description="essa BMW também tem 4 pneus" />
+                    <ProductCard
+                    img="/bmw2.png"
+                    title="BMW mais bonita ainda"
+                    price={151515}
+                    description="essa bmw possui 4 pneus" />
+
+                <ProductCard
+                    img="/bmw3.png"
+                    title="BMW modelo novo 2030"
+                    price={151515}
+                    description="essa BMW também tem 4 pneus" />
+
+            </section>
+        </main>
     )
 }
--- components/ProductCard/index.tsx
+++ components/ProductCard/index.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { ProductCardInterface } from "./interface";
+import './style.css';
 
 
 function ProductCard({ img, title, price, description }: ProductCardInterface) {
     return (
         <div className="product-card">
-            <Image src={img} alt={title} width={150} height={150} />
+            <Image src={img} alt={title} width={350} height={200} />
             <h2>{title}</h2>
             <p>{description}</p>
-            <span>{price}</span> </div>
-    )}
+            <p>R$:{price}</p>
+        </div>
+    )
+}
 
 export default ProductCard;
