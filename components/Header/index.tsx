2013f7c562e32923b28ab3be45e226299a36ccdd
yagoluucas <yago543@gmail.com>
2024-06-22 07:41:46 -0300

    mudado menu para funcionar apenas no client

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,39 +2,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import './syle.css'
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header() {
     const [iconeMenu, setIconeMenu] = useState('/icone-abrir.png')
     const menu = useRef<HTMLUListElement>(null)
     function setMenu() {
-        if(menu.current) {
-            if(iconeMenu === '/icone-abrir.png') {
+        if (menu.current) {
+            if (iconeMenu === '/icone-abrir.png') {
                 menu.current.style.display = 'flex'
                 setIconeMenu('/icone-fechar.png')
-            }else {
+            } else {
                 menu.current.style.display = 'none'
                 setIconeMenu('/icone-abrir.png')
             }
         }
     }
 
-    window.addEventListener('resize', () => {
-        if (menu.current) {
-            if(window.innerWidth > 900) {
-                menu.current.style.display = 'flex'
-                setIconeMenu('/icone-fechar.png')
-            }else {
-                setIconeMenu('/icone-abrir.png')
-                menu.current.style.display = 'none'
-            }
+    function changeMenu() {
+        if (typeof window !== 'undefined') {
+            window.addEventListener('resize', () => {
+                if (menu.current) {
+                    if (window.innerWidth > 900) {
+                        menu.current.style.display = 'flex'
+                        setIconeMenu('/icone-fechar.png')
+                    } else {
+                        setIconeMenu('/icone-abrir.png')
+                        menu.current.style.display = 'none'
+                    }
+                }
+            })
         }
-    })   
+    }
+
+    useEffect(() => {
+        changeMenu()
+    }, [])
+
+
     return (
         <header className="header-pagina">
-            <Link href={'/'}><Image className="img-logo" src={'/logo.png'} alt="Logo da empresa" width={150} height={150}/></Link>
+            <Link href={'/'}><Image className="img-logo" src={'/logo.png'} alt="Logo da empresa" width={150} height={150} /></Link>
             <nav>
-                <Image className="img-menu-mobile" onClick={setMenu} src={iconeMenu} alt="Menu" width={40} height={40}/>
+                <Image className="img-menu-mobile" onClick={setMenu} src={iconeMenu} alt="Menu" width={40} height={40} />
                 <ul ref={menu} className="nav-header">
                     <li><Link className="link" href={'/contato'}>contato</Link></li>
                     <li><Link className="link" href={'/produtos'}>produtos</Link></li>
